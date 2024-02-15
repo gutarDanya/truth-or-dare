6fbfdf18bf1a8773932b5a0d12daee7bd892c56c
Daniil_Ushakov <gutardanya@gmail.com>
2024-02-16 00:54:05 +0400

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,22 +5,22 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.columnContaienr} >
-                <h4 className={styles.columnHeader}>Связь с автором</h4>
-                <a target='_blank' href='' className={styles.link}></a>
-                <a target='_blank' href='' className={styles.link}></a>
-                <a target='_blank' href='' className={styles.link}></a>
-                <a target='_blank' href='' className={styles.link}></a>
-                <a target='_blank' href='' className={styles.link}></a>
+                <div className={styles.column}>
+                    <h4 className={styles.columnHeader}>Связь с автором</h4>
+                    <a target='_blank' href='https://vk.com/gutardanya' className={styles.link}>Вконтакте</a>
+                    <a target='_blank' href='' className={styles.link}>Instagram</a>
+                    <a target='_blank' href="https://tlgg.ru/@ush_dan" className={styles.link}>@ush_dan</a>
+                    <a target='_blank' href='https://api.whatsapp.com/send?phone=79011070093' className={styles.link}>WhatsApp</a>
+                </div>
+                <div className={styles.column}>
+                    <h4 className={styles.columnHeader}>Социальные сети проекта</h4>
+                    <a target='_blank' href='https://github.com/gutarDanya/truth-or-dare' className={styles.link}>GitHub</a>
+                    <a target='_blank' href='#' className={styles.link}>Телеграм</a>
+                    <a target='_blank' href='#' className={styles.link}>Instagram</a>
+                    <a target='_blank' href='#' className={styles.link}>Вконтакте</a>
+                </div>
             </div>
-            <div className={styles.columnContaienr}>
-                <h4 className={styles.columnHeader}>Социальные сети проекта</h4>
-                <a target='_blank' href='' className={styles.link}></a>
-                <a target='_blank' href='' className={styles.link}></a>
-                <a target='_blank' href='' className={styles.link}></a>
-                <a target='_blank' href='' className={styles.link}></a>
-                <a target='_blank' href='' className={styles.link}></a>
-            </div>
-            <strong>© Daniil Ushakov</strong>
+            <strong className={styles.author}>© Daniil Ushakov</strong>
         </footer>
     )
 }
